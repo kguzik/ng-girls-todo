29d2801cc51b57357df96095ce3beba180726347
Karolina Guzik <kguzik@Mareks-MacBook-Air.local>
2019-09-12 19:50:45 +0200

    Todo list app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1 class="app-title">
+      Welcome to {{ title }}
+    </h1>
+    <app-list-manager></app-list-manager>
+
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'app';
+  todoList: TodoItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+  addItem(value: string) {
+    this.todoList.push({ title: value });
+  }
 }
